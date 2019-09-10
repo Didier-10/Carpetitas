c31ed5700c49f90b51a8c4ddafcf0e4f24ce8d02
Didier-10 <54007348+Didier-10@users.noreply.github.com>
2019-09-09 19:01:27 -0500

    Add files via upload

---
+++ SeguidorDeVistas/SeguidorDeVistas/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeguidorDeVistas
+//
+//  Created by 2020-1 on 9/9/19.
+//  Copyright © 2019 unam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func salidaDeVista(unwindSegue: UIStoryboardSegue)
+    {
+}
+
+}
